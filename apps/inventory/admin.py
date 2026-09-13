9b7cdfdf50100e7a2e022ba009beaff852e41ee7
HABIBA204 <habibaebrahim204@gmail.com>
2026-09-13 08:47:41 +0300

    feat: register inventory models in Django admin for intial control panel

--- apps/inventory/admin.py
+++ apps/inventory/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Supplier, Product, Purchase, PurchaseItem, Sale, SaleItem
+
+admin.site.register(Supplier)
+admin.site.register(Product)
+
+
+admin.site.register(Purchase)
+admin.site.register(PurchaseItem)
+admin.site.register(Sale)
+admin.site.register(SaleItem)
 
 # Register your models here.
