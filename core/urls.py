687669b3f9d31e9a41cc6d4ac1fb5a74bc05aee4
HABIBA204 <habibaebrahim204@gmail.com>
2026-09-17 18:04:45 +0300

    feat: add authentication forms and templates

--- core/urls.py
+++ core/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/agent/", include("apps.ai_agent.urls"))
+    path("api/agent/", include("apps.ai_agent.urls")),
+    path('auth/', include('apps.authentication.urls')),
 ]
