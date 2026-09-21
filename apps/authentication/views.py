7d69214fea254a55f1a8541c433e9ee8952ac64e
Youssef-Zahran-Git <yosefzahran111@gmail.com>
2026-09-21 22:27:15 +0300

    real last dance

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,8 +1,8 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
+
 def login_view(request):
-    error=None
     if request.method == 'POST':
         username_input = request.POST.get('username')
         password_input = request.POST.get('password')
@@ -10,10 +10,22 @@ def login_view(request):
         user = authenticate(request, username=username_input, password=password_input)
         
         if user is not None:
-           login(request, user)
-           return redirect('dashboard')
-    else:
-         return render(request, 'authentication/login.html', {'error': 'Username or password is Wroung'})
-        
+            login(request, user)
+            
+            if user.groups.filter(name='Owner').exists():
+                return redirect('/')
+            else:
+                return redirect('/')
+        else:
+            return render(request, 'authentication/login.html', {'error': 'اسم المستخدم أو كلمة المرور غير صحيحة'})
+            
+    return render(request, 'authentication/login.html')
+
+
+def logout_view(request):
+    # بيمسح جلسة اليوزر الحالية (الكوكي بتاعة تسجيل الدخول)
+    # وبعدها أي محاولة دخول على الموقع هتتحول لصفحة اللوجين تاني
+    logout(request)
+    return redirect('login')
 
 # Create your views here.
